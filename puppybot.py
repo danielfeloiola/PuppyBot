c6998da1e5a1e5fc23ecb5ec94bcee5317c4dcd8
Daniel Loiola <Daniel@Daniels-iMac.local>
2019-11-02 18:00:58 -0300

    tensorflow backend

--- puppybot.py
+++ puppybot.py
@@ -1,11 +1,9 @@
 '''
- _______  __   __  _______  _______  __   __  _______  _______  _______
-|       ||  | |  ||       ||       ||  | |  ||  _    ||       ||       |
-|    _  ||  | |  ||    _  ||    _  ||  |_|  || |_|   ||   _   ||_     _|
-|   |_| ||  |_|  ||   |_| ||   |_| ||       ||       ||  | |  |  |   |
-|    ___||       ||    ___||    ___||_     _||  _   | |  |_|  |  |   |
-|   |    |       ||   |    |   |      |   |  | |_|   ||       |  |   |
-|___|    |_______||___|    |___|      |___|  |_______||_______|  |___|
+ ______   __  __     ______   ______   __  __     ______     ______     ______
+/\  == \ /\ \/\ \   /\  == \ /\  == \ /\ \_\ \   /\  == \   /\  __ \   /\__  _\
+\ \  _-/ \ \ \_\ \  \ \  _-/ \ \  _-/ \ \____ \  \ \  __<   \ \ \/\ \  \/_/\ \/
+ \ \_\    \ \_____\  \ \_\    \ \_\    \/\_____\  \ \_____\  \ \_____\    \ \_\
+  \/_/     \/_____/   \/_/     \/_/     \/_____/   \/_____/   \/_____/     \/_/
 
 '''
 ########################################################################
@@ -20,12 +18,26 @@ import os
 import json
 import tweepy
 
+# stuff
 from time import sleep
 from ssl import SSLError
 from requests.exceptions import Timeout, ConnectionError
 from urllib3.exceptions import ReadTimeoutError
+
+# img stuff
+from io import BytesIO
+from PIL import Image
+from PIL import ImageFile
+
+# db stuff
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+
+# requests
+import requests
+
+# TF detector
+from dog_app3 import dog_detector
 
 # make a database
 engine = create_engine(os.getenv("DATABASE_URI"))
@@ -41,101 +53,149 @@ consumer_secret = os.getenv("CONSUMER_SECRET")
 access_token = os.getenv("ACCESS_TOKEN")
 access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
 
+# api setup
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 
 def check_safety(status):
-#check if a tweet is violent or contains porn
+    '''
+    check if a tweet is adequate
+    Looks for bad words on username, text and description
+    checks if threre is a image on the tweet
+    '''
 
     # get tweet out of json package
     tweet_json = json.dumps(status._json)
     tweet = json.loads(tweet_json)
 
-    # MAKE SAFETY LIST
+    # get words from db
     safe_list_db = db.execute("SELECT * FROM slist").fetchall()
 
     # make a list with bad words
     slist = []
+
+    # add the words from the db to the list
     for item in safe_list_db:
         slist.append(item[0])
 
+    # start checking if its a safe tweet
+    # returns false if the test fails
+
     # Make sure it's not a RT
-    if (not status.retweeted) and ('RT @' not in status.text) and ('media' in status.entities):
+    if status.retweeted:
+        print('------------------------------------------')
+        print("RT!")
+        print('------------------------------------------')
+        return False
+    if 'RT @' in status.text:
+        print('------------------------------------------')
+        print("RT!")
+        print('------------------------------------------')
+        return False
 
-        # check for porn or kpop on the tweet
-        if not any(word in status.text.lower() for word in slist):
+    # check if there is indeed a picture in it
+    if ('media' in status.entities):
 
-            # check for porn on the user description
-            if tweet['user']['description'] != None:
+        # looking for bad words
 
-                if not any(word in tweet['user']['description'].lower() for word in slist):
-
-                    # check user name
-                    if not any(word in tweet['user']['name'].lower() for word in slist):
-
-                        if len(status.text) > 6:
-
-                            for char in tweet['user']['description']:
-                                if ord(char) > 256:
-                                    return False
-
-                            for char in tweet['user']['name']:
-                                if ord(char) > 256:
-                                    return False
-
-                            if tweet['possibly_sensitive'] == False:
-
-                                # say its safe
-                                print('Got a safe tweet')
-                                print(status.text)
-                                return True
-
-                            # SHOW REMOVED TWEETS
-                            # if the tweet has kpop or porn on the username:
-                            else:
-                                print('FOUND A UNSAFE ONE!!! - possibly_sensitive')
-                                print(status.text)
-                                print(tweet['user']['name'])
-                                print(tweet['user']['description'])
-                                return False
-
-                        else:
-                            print("status is too short")
-                            print(status.text)
-                    # SHOW REMOVED TWEETS
-                    # if the tweet has kpop or porn on the username:
-                    else:
-                        print('FOUND A UNSAFE ONE!!! - NAME')
-                        print(status.text)
-                        print(tweet['user']['name'])
-                        print(tweet['user']['description'])
-                        return False
-
-                # if the tweet has kpop or porn on the user description:
-                else:
-                    print('FOUND A UNSAFE ONE!!! - DESCRIPTION')
-                    print(status.text)
-                    print(tweet['user']['name'])
-                    print(tweet['user']['description'])
-                    return False
+        # check for p*rn or kpop on the tweet
+        if any(word in status.text.lower() for word in slist):
+            print('')
+            print('------------------------------------------')
+            print('')
+            print('unsafe - text')
+            print(status.text)
+            #print(tweet['user']['name'])
+            #print(tweet['user']['description'])
+            print('')
+            print('------------------------------------------')
+            print('')
+            return False
 
 
-            # if the user profile has no description:
-            else:
-                print('FOUND A UNSAFE ONE!!! - EMPTY DESCRIPTION')
+        # check user name
+        if any(word in tweet['user']['name'].lower() for word in slist):
+            print('')
+            print('------------------------------------------')
+            print('')
+            print('unsafe - name')
+            #print(status.text)
+            print(tweet['user']['name'])
+            #print(tweet['user']['description'])
+            print('')
+            print('------------------------------------------')
+            print('')
+            return False
+
+        #if any(word in tweet['user']['description'].lower() for word in slist):
+        #print('')
+        #print('------------------------------------------')
+        #print('')
+        #    print('unsafe -> description')
+        #    #print(status.text)
+        #    #print(tweet['user']['name'])
+        #    print(tweet['user']['description'])
+        #    print('')
+        #    print('------------------------------------------')
+        #    print('')
+        #    return False
+
+        # if the tweet is not marked as sensitive:
+        if tweet['possibly_sensitive'] == False:
+            print('not sensitive')
+
+            return True
+
+    else:
+
+        print('------------------------------------------')
+        print('')
+        print("no image on the tweet")
+        print('')
+        print('------------------------------------------')
+
+        return False
+
+
+def check_dog(status):
+    '''
+    Checks if there is a dog on the image using TensorFlow
+    '''
+
+    for image in status.entities['media']:
+
+        url = image['media_url']
+        print("URL ------>" + url)
+        filename = 'temp.png'
+
+        # send a get request to get the image
+        request = requests.get(url, stream=True)
+        if request.status_code == 200:
+
+            # read data from downloaded bytes and returns a PIL.Image.Image object
+            i = Image.open(BytesIO(request.content))
+
+            # Saves the image under the given filename
+            i.save(filename)
+
+            # call the detector function
+            dog = dog_detector("temp.png")
+
+            # if a dog is detected return true
+            # else return false
+            if dog == True:
+                # say its safe
+                print('GOT A DOGGO!')
                 print(status.text)
-                print(tweet['user']['name'])
-                print(tweet['user']['description'])
+                return True
+            else:
                 return False
 
-        # if the tweet has kpop or porn on the text:
+        # if the request for the image fails return false
         else:
-            print('FOUND A UNSAFE ONE!!! - TEXT')
-            print(status.text)
-            print(tweet['user']['name'])
-            print(tweet['user']['description'])
+            print("unable to download image")
             return False
 
 
@@ -151,16 +211,20 @@ class MyStreamListener(tweepy.StreamListener):
         # if its indeed safe
         if check == True:
 
-            # tries to retweet the tweet - or just gives up
-            try:
-                print('about to retweet')
-                api.retweet(status.id)
-                print('RETWEETED!!!')
-                sleep(1800) # for tweeting every 50 mins
-                print('done sleeping!')
-            except:
-                print('passing...')
-                pass
+            dog_on_image = check_dog(status)
+            if dog_on_image == True:
+
+
+                # tries to retweet the tweet - or just gives up
+                try:
+                    print('about to retweet')
+                    api.retweet(status.id)
+                    print('RETWEETED!!!')
+                    sleep(3600) # for tweeting every 30 mins
+                    print('done sleeping!')
+                except:
+                    print('passing...')
+                    pass
 
 
     # if there is any error
@@ -199,7 +263,7 @@ while not stream.running:
         print(e)
 
     finally:
-        # wars about the crash
+        # warns about the crash
         print("Stream has crashed. System will restart twitter stream soon!")
 
 # if error escapes jail...
