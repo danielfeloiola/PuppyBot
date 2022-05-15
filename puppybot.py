b7eaea823289d90f9963ad486d37e9a5254f9fe6
Daniel Loiola <danf10@me.com>
2022-05-15 15:15:57 -0300

    reorganized files

--- puppybot.py
+++ puppybot.py
@@ -1,14 +1,14 @@
 '''
- ______   __  __     ______   ______   __  __     ______     ______     ______
-/\  == \ /\ \/\ \   /\  == \ /\  == \ /\ \_\ \   /\  == \   /\  __ \   /\__  _\
-\ \  _-/ \ \ \_\ \  \ \  _-/ \ \  _-/ \ \____ \  \ \  __<   \ \ \/\ \  \/_/\ \/
- \ \_\    \ \_____\  \ \_\    \ \_\    \/\_____\  \ \_____\  \ \_____\    \ \_\
-  \/_/     \/_____/   \/_/     \/_/     \/_____/   \/_____/   \/_____/     \/_/
-
+ ____                                      ____            __      
+/\  _`\                                   /\  _`\         /\ \__   
+\ \ \L\ \__  __  _____   _____   __  __   \ \ \L\ \    ___\ \ ,_\  
+ \ \ ,__/\ \/\ \/\ '__`\/\ '__`\/\ \/\ \   \ \  _ <'  / __`\ \ \/  
+  \ \ \/\ \ \_\ \ \ \L\ \ \ \L\ \ \ \_\ \   \ \ \L\ \/\ \L\ \ \ \_ 
+   \ \_\ \ \____/\ \ ,__/\ \ ,__/\/`____ \   \ \____/\ \____/\ \__\
+    \/_/  \/___/  \ \ \/  \ \ \/  `/___/> \   \/___/  \/___/  \/__/
+                   \ \_\   \ \_\     /\___/                        
+                    \/_/    \/_/     \/__/                                                                                           
 '''
-
-
-
 ########################################################################
 # A BOT THAT RETWETS IMAGES OF PUPPIES                                 #
 # version: idk, probably a million?                                    #
@@ -18,39 +18,23 @@
 
 # import libraries needed
 import os
-import json
 import tweepy
-
-# stuff
 from time import sleep
 from ssl import SSLError
 from requests.exceptions import Timeout, ConnectionError
 from urllib3.exceptions import ReadTimeoutError
 
-# img stuff
-from io import BytesIO
-from PIL import Image
-from PIL import ImageFile
 
-# db stuff
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
+# functions from the package to check tweets
+from helpers import check_safety, check_dog
 
-# requests
-import requests
 
-# TF detector
-from dog_app import dog_detector
-
-# make a database
-engine = create_engine(os.getenv("DATABASE_URI"))
-db = scoped_session(sessionmaker(bind=engine))
-
-# create an OAuthHandler
+# Get Twitter keys
 if not os.getenv("CONSUMER_KEY"):
     raise RuntimeError("CONSUMER_KEY is not set")
 
-# create twitter api
+
+# twitter api credentials
 consumer_key = os.getenv("CONSUMER_KEY")
 consumer_secret = os.getenv("CONSUMER_SECRET")
 access_token = os.getenv("ACCESS_TOKEN")
@@ -62,172 +46,25 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 
-def check_safety(status):
-    '''
-    check if a tweet is adequate
-    Looks for bad words on username, text and description
-    checks if threre is a image on the tweet
-    '''
-
-    # get tweet out of json package
-    tweet_json = json.dumps(status._json)
-    tweet = json.loads(tweet_json)
-
-    # get words from db
-    safe_list_db = db.execute("SELECT * FROM slist").fetchall()
-
-    # make a list with bad words
-    slist = []
-
-    # add the words from the db to the list
-    for item in safe_list_db:
-        slist.append(item[0])
-
-    # start checking if its a safe tweet
-    # returns false if the test fails
-
-    # Make sure it's not a RT
-    if status.retweeted:
-        print('------------------------------------------')
-        print("RT!")
-        print('------------------------------------------')
-        return False
-    if 'RT @' in status.text:
-        print('------------------------------------------')
-        print("RT!")
-        print('------------------------------------------')
-        return False
-
-    # check if there is indeed a picture in it
-    if ('media' in status.entities):
-
-        # looking for bad words
-
-        # check for p*rn or kpop on the tweet
-        if any(word in status.text.lower() for word in slist):
-            print('')
-            print('------------------------------------------')
-            print('')
-            print('unsafe - text')
-            print(status.text)
-            #print(tweet['user']['name'])
-            #print(tweet['user']['description'])
-            print('')
-            print('------------------------------------------')
-            print('')
-            return False
-
-
-        # check user name
-        if any(word in tweet['user']['name'].lower() for word in slist):
-            print('')
-            print('------------------------------------------')
-            print('')
-            print('unsafe - name')
-            #print(status.text)
-            print(tweet['user']['name'])
-            #print(tweet['user']['description'])
-            print('')
-            print('------------------------------------------')
-            print('')
-            return False
-
-        #if any(word in tweet['user']['description'].lower() for word in slist):
-        #print('')
-        #print('------------------------------------------')
-        #print('')
-        #    print('unsafe -> description')
-        #    #print(status.text)
-        #    #print(tweet['user']['name'])
-        #    print(tweet['user']['description'])
-        #    print('')
-        #    print('------------------------------------------')
-        #    print('')
-        #    return False
-
-        # if the tweet is not marked as sensitive:
-        if tweet['possibly_sensitive'] == False:
-            print('not sensitive')
-
-            return True
-
-    else:
-
-        print('------------------------------------------')
-        print('')
-        print("no image on the tweet")
-        print('')
-        print('------------------------------------------')
-
-        return False
-
-
-def check_dog(status):
-    '''
-    Checks if there is a dog on the image using TensorFlow
-    '''
-
-    for image in status.entities['media']:
-
-        url = image['media_url']
-        print("URL ------>" + url)
-        filename = 'temp.png'
-
-        # send a get request to get the image
-        request = requests.get(url, stream=True)
-        if request.status_code == 200:
-
-            # read data from downloaded bytes and returns a PIL.Image.Image object
-            i = Image.open(BytesIO(request.content))
-
-            # Saves the image under the given filename
-            i.save(filename)
-
-            # call the detector function
-            dog = dog_detector("temp.png")
-
-            # if a dog is detected return true
-            # else return false
-            if dog == True:
-                # say its safe
-                print('GOT A DOGGO!')
-                print(status.text)
-                return True
-            else:
-                return False
-
-        # if the request for the image fails return false
-        else:
-            print("unable to download image")
-            return False
-
-
-# Creates a class for the listener
-class MyStreamListener(tweepy.StreamListener):
+# Creates a class for the stream
+class MyStream(tweepy.Stream):
 
     # if a new tweet gets found out...
     def on_status(self, status):
 
         # check if tweet is safe
-        check = check_safety(status)
+        if check_safety(status) and check_dog(status):
 
-        # if its indeed safe
-        if check == True:
+            # tries to retweet the tweet - or just gives up
+            try:
+                print('\n*** ABOUT TO RETWEET ***\n')
+                api.retweet(status.id)
+                print('\n*** RETWEETED! ***\n')
+                sleep(3600) # for tweeting every 30 mins
 
-            dog_on_image = check_dog(status)
-            if dog_on_image == True:
-
-
-                # tries to retweet the tweet - or just gives up
-                try:
-                    print('about to retweet')
-                    api.retweet(status.id)
-                    print('RETWEETED!!!')
-                    sleep(3600) # for tweeting every 30 mins
-                    print('done sleeping!')
-                except:
-                    print('passing...')
-                    pass
+            except:
+                print('\n*** RETWEET FAILED ***\n')
+                pass
 
 
     # if there is any error
@@ -235,39 +72,35 @@ class MyStreamListener(tweepy.StreamListener):
 
         # Being rate limited for making too many requests.
         if status_code == 420:
-            print('ERROR 420: Too many requests')
-            sleep(300)
-            print('Back to work!')
+            print('\n*** ERROR 420: Too many requests ***\n')
+            sleep(3600)
+            print('\n*** Back to work! ***\n')
             return True
 
         # Cannot retweet the same Tweet more than once
         if status_code == 327:
-            print('ERROR 327: You have already retweeted this Tweet')
+            print('\n*** ERROR 327: You have already retweeted this Tweet ***\n')
             return True
 
 
-# Start a stream listener to look for tweets with puppies
-myStreamListener = MyStreamListener()
-stream = tweepy.Stream(auth, myStreamListener)
+# start the kitten machine gun!
+stream = MyStream(
+    consumer_key, consumer_secret,
+    access_token, access_token_secret
+)
 
+# error checking
 while not stream.running:
     try:
-        # start stream
-        print("Started listening to stream...")
-        stream.filter(track=['puppy'])
+        print("\n*** Started listening to stream ***\n")
+        stream.filter(track=['puppy '])
 
     except (Timeout, SSLError, ReadTimeoutError, ConnectionError) as e:
-        # if there is a connection error
         print("Network error. Keep calm and carry on.", str(e))
         sleep(300)
 
     except Exception as e:
-        # if there is another kind of error
         print(e)
 
     finally:
-        # warns about the crash
         print("Stream has crashed. System will restart twitter stream soon!")
-
-# if error escapes jail...
-print("This error was so bad I have no idea what it was!")
